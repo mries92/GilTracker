3365626750bc0709d85d0110fa34212096c1ff98
Mike Ries <mries92@gmail.com>
2021-09-08 16:39:22 -0400

    Typescript switch.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Chart } from '../node_modules/chart.js/dist/chart.js'
 
 window.addEventListener('DOMContentLoaded', () => {
-    var ctx = document.getElementById('chart').getContext('2d');
+    var ctx = (<HTMLCanvasElement>document.getElementById('chart')).getContext('2d');
     var chart = new Chart(ctx, {
         type: 'line',
         data: {
