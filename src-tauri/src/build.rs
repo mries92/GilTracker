dda0cba07706e1a168eb322aeadf7a2d6b2811a1
Mike Ries <mries92@gmail.com>
2021-09-12 15:59:32 -0400

    Switched to official microsoft bindings for winapi.

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-  tauri_build::build()
+  tauri_build::build();
+  windows::build! {
+    Windows::Win32::System::ProcessStatus::{K32EnumProcessModules},
+    Windows::Win32::System::Threading::{OpenProcess},
+    Windows::Win32::Foundation::{BOOL, HINSTANCE, HWND, HANDLE},
+    Windows::Win32::UI::WindowsAndMessaging::{FindWindowA},
+  };
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,24 @@
 
 extern crate benfred_read_process_memory as read_process_memory;
 
+mod bindings {
+  windows::include_bindings!();
+}
+
+use bindings::{
+  Windows::Win32::System::ProcessStatus::{K32EnumProcessModules},
+  Windows::Win32::System::Threading::{OpenProcess, PROCESS_ALL_ACCESS},
+  Windows::Win32::Foundation::{BOOL, HINSTANCE, HWND, HANDLE},
+  Windows::Win32::UI::WindowsAndMessaging::{FindWindowA},
+};
+
+/*
+#[cfg(windows)] extern crate winapi;
+use winapi::um::psapi::{EnumProcessModules, GetModuleFileNameExA};
+use winapi::um::processthreadsapi::OpenProcess;
+use winapi::shared::minwindef::{DWORD, BOOL, HMODULE, MAX_PATH};
+use winapi::um::winnt::{PROCESS_ALL_ACCESS, HANDLE, LPSTR};*/
+
 use read_process_memory::{copy_address, CopyAddress, Pid, ProcessHandle};
 use std::{
   convert::TryInto,
@@ -20,6 +38,7 @@ fn main() {
         let mut sys = System::new_all();
         let run = true;
         let mut found = false;
+        let mut base_offset: usize = 0;
         let mut process_id: usize = 1 as usize;
         let process_scan_interval = Duration::from_secs(3);
         let _currency_scan_interval = Duration::from_secs(10);
@@ -33,7 +52,11 @@ fn main() {
               if process.name() == "ffxiv_dx11.exe" {
                 found = true;
                 process_id = *pid;
-                println!("Found game process: {}", process_id);
+                base_offset = process.memory() as usize;
+                println!(
+                  "Found game process... ID: {}, Offset: {}",
+                  process_id, base_offset
+                );
                 break;
               }
             }
@@ -45,18 +68,21 @@ fn main() {
           }
           // Scan for values
           else {
+            let handle = get_handle(process_id as u32).expect("Not sure how this happened.");
+            let module = get_module(handle);
+
             let start = Instant::now();
-            let bytes = read_memory(process_id as Pid, 0x200F3161800 + 0x78, 8).unwrap();
-            let str : String = hex::encode(bytes);
+            let bytes = read_memory(process_id as Pid, base_offset + 0x78, 8).unwrap();
+            let str: String = hex::encode(bytes);
             println!("Wallet address: 0x{}", str);
-            let address : usize = usize::from_str_radix(&str, 16).unwrap().try_into().unwrap();
-            let bytes : Vec<u8> = read_memory(process_id as Pid, address + 0xC, 4).unwrap();
-            let str : String = hex::encode(bytes);
-            let gil : i32 = i32::from_str_radix(&str, 16).unwrap().try_into().unwrap();
+            let address: usize = usize::from_str_radix(&str, 16).unwrap().try_into().unwrap();
+            let bytes: Vec<u8> = read_memory(process_id as Pid, address + 0xC, 4).unwrap();
+            let str: String = hex::encode(bytes);
+            let gil: i32 = i32::from_str_radix(&str, 16).unwrap().try_into().unwrap();
             println!("Gil: {}\n\n", gil);
             let runtime = start.elapsed();
             if let Some(remaining) = process_scan_interval.checked_sub(runtime) {
-                thread::sleep(remaining);
+              thread::sleep(remaining);
             }
           }
         }
@@ -74,4 +100,35 @@ fn read_memory(pid: Pid, address: usize, size: usize) -> io::Result<Vec<u8>> {
   _bytes.reverse(); // Flip the bytes so they are easier to work with (little endian to big)
   println!("Read {} bytes", size);
   Ok(_bytes)
+}
+
+// Get a win32 handle to a process by process ID
+fn get_handle(pid: u32) -> io::Result<HANDLE> {
+  let handle: HANDLE;
+  unsafe {
+    handle= OpenProcess(PROCESS_ALL_ACCESS, BOOL::from(false), pid);
+  }
+  Ok(handle)
+}
+
+// Get the base module of a process by process ID
+fn get_module(handle: HANDLE) -> io::Result<u32> {
+
+  let mut hmods: *mut HINSTANCE = std::ptr::null_mut();
+  let mut cbneeded: *mut u32 = std::ptr::null_mut();
+  unsafe {
+    if K32EnumProcessModules(handle, hmods, 1024, cbneeded) == true {
+      /*
+      // Loop through each module until we find the game
+      let mut i = 0;
+      while i < (cbneeded / std::mem::size_of::<HMODULE>() as u32){
+        let filename: [LPSTR; MAX_PATH];
+        if GetModuleFileNameExA(handle.0, hmods, filename, std::mem::size_of::<filename>() / std::mem::size_of::<LPSTR>()) == 1 {
+  
+        }
+        i += 1;
+      }*/
+    }
+  }
+  Ok((32))
 }
