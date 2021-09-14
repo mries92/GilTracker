4394e8a7543edcaee49a99c062e7f0a09e17abf1
Mike Ries <mries92@gmail.com>
2021-09-14 17:53:21 -0400

    Commiting as a checkpoint. Got Arc 'working'

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -1,10 +1,8 @@
 fn main() {
   tauri_build::build();
   windows::build! {
-    Windows::Win32::System::Diagnostics::Debug::{GetLastError},
     Windows::Win32::System::ProcessStatus::{K32EnumProcessModules},
     Windows::Win32::System::Threading::{OpenProcess},
-    Windows::Win32::Foundation::{BOOL, HINSTANCE, HWND, HANDLE},
-    Windows::Win32::UI::WindowsAndMessaging::{FindWindowA},
+    Windows::Win32::Foundation::{BOOL, HINSTANCE, HANDLE},
   };
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,125 +10,151 @@ mod bindings {
 }
 
 use bindings::{
-  Windows::Win32::System::Diagnostics::Debug::{GetLastError},
-  Windows::Win32::System::ProcessStatus::{K32EnumProcessModules},
-  Windows::Win32::System::Threading::{OpenProcess, PROCESS_ALL_ACCESS, PROCESS_VM_READ, PROCESS_QUERY_INFORMATION},
-  Windows::Win32::Foundation::{BOOL, HINSTANCE, HWND, HANDLE},
-  Windows::Win32::UI::WindowsAndMessaging::{FindWindowA},
+  Windows::Win32::Foundation::{BOOL, HANDLE, HINSTANCE},
+  Windows::Win32::System::ProcessStatus::K32EnumProcessModules,
+  Windows::Win32::System::Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
 };
 
-use read_process_memory::{copy_address, CopyAddress, Pid, ProcessHandle};
+use read_process_memory::{copy_address, Pid, ProcessHandle};
 use std::{
   convert::TryInto,
-  io, thread,
+  io,
+  mem::MaybeUninit,
+  ops::Deref,
+  sync::{Arc, Mutex},
+  thread,
   time::{Duration, Instant},
-  mem::{MaybeUninit}
 };
 use sysinfo::{ProcessExt, System, SystemExt};
 
 fn main() {
+  let scanner = Arc::new(Mutex::new(Scanner::new()));
   tauri::Builder::default()
-    .setup(|_| {
-      let _scheduler = thread::spawn(|| {
-        let mut sys = System::new_all();
-        let run = true;
-        let mut found = false;
-        let mut base_offset: usize = 0;
-        let mut process_id: usize = 1 as usize;
-        let process_scan_interval = Duration::from_secs(3);
-        let _currency_scan_interval = Duration::from_secs(10);
-        // Main background scanning loop
-        while run == true {
-          sys.refresh_processes();
-          // Search for game process
-          if !found {
-            let start = Instant::now();
-            for (pid, process) in sys.processes() {
-              if process.name() == "ffxiv_dx11.exe" {
-                found = true;
-                process_id = *pid;
-                base_offset = process.memory() as usize;
-                println!(
-                  "Found game process... ID: {}, Offset: {}",
-                  process_id, base_offset
-                );
-                break;
-              }
-            }
-            let runtime = start.elapsed();
-            // If there is time left in the scheduler, sleep thread
-            if let Some(remaining) = process_scan_interval.checked_sub(runtime) {
-              thread::sleep(remaining);
-            }
-          }
-          // Scan for values
-          else {
-            let handle = get_handle(process_id as u32).expect("Not sure how this happened.");
-            let module : u64 = get_module(handle).expect("Module not found.");
-            println!("Module Address: {}", module);
-
-            let start = Instant::now();
-
-            // Base
-            let bytes = read_memory(process_id as Pid, module as usize + 0x01DD4358, 8).unwrap();
-            let str: String = hex::encode(bytes);
-            println!("Static offset address: 0x{}", str);
-            let address: usize = usize::from_str_radix(&str, 16).unwrap().try_into().unwrap();
-
-            // First offset
-            let bytes: Vec<u8> = read_memory(process_id as Pid, address + 0x78, 8).unwrap();
-            let str: String = hex::encode(bytes);
-            let address: usize = usize::from_str_radix(&str, 16).unwrap().try_into().unwrap();
-
-            // Final offset
-            let bytes: Vec<u8> = read_memory(process_id as Pid, address + 0xC, 4).unwrap();
-            let gil: u32 = u32::from_be_bytes(bytes.try_into().expect("Should always have a value"));
-            println!("Gil: {}\n\n", gil);
-
-            let runtime = start.elapsed();
-            if let Some(remaining) = process_scan_interval.checked_sub(runtime) {
-              thread::sleep(remaining);
-            }
-          }
-        }
-      });
-      Ok(())
-    })
+    .setup(|_| Ok(()))
+    .manage(scanner)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
-// Read an array of bytes from a memory location
-fn read_memory(pid: Pid, address: usize, size: usize) -> io::Result<Vec<u8>> {
-  let handle: ProcessHandle = pid.try_into()?;
-  let mut _bytes = copy_address(address, size, &handle)?;
-  _bytes.reverse(); // Flip the bytes so they are easier to work with (little endian to big)
-  Ok(_bytes)
+#[tauri::command]
+fn get_gil(state: tauri::State<Scanner>) -> u32 {
+  return state.get_gil();
 }
 
-// Get a win32 handle to a process by process ID
-fn get_handle(pid: u32) -> io::Result<HANDLE> {
-  let mut handle: HANDLE;
-  unsafe {
-    handle = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, BOOL::from(false), pid);
-  }
-  Ok(handle)
-}
-
-// Get the base module of a process by process ID
-fn get_module(handle: HANDLE) -> io::Result<u64> {
-  let mut hmods;
-  unsafe {
-    hmods = MaybeUninit::<[MaybeUninit<HINSTANCE>; 1024]>::uninit().assume_init();
-  }
-  let ptr = hmods.as_mut_ptr() as *mut HINSTANCE;
-  let mut cbneeded: u32 = 0;
-  let mut module_id: u64 = 0;
-  unsafe {
-    if K32EnumProcessModules(handle, ptr, 1024, &mut cbneeded) == BOOL::from(true) {
-      // Base module is always first
-      module_id = hmods[0].assume_init().0 as u64;
+// Start the thread responsible for attempting to attach to the game
+// process. Thread dies after game process is found.
+fn scanner_attach_thread(scanner: Arc<Mutex<Scanner>>) {
+  let scanner_cpy = scanner.clone();
+  std::thread::spawn(move || {
+    let mut sys = System::new_all();
+    let mut run = true;
+    let mut found = false;
+    let process_scan_interval = Duration::from_secs(3);
+    // Main background scanning loop
+    while run == true {
+      sys.refresh_processes();
+      // Search for game process
+      if !found {
+        let start = Instant::now();
+        for (pid, process) in sys.processes() {
+          if process.name() == "ffxiv_dx11.exe" {
+            found = true;
+            let handle = Scanner::get_handle(*pid as u32).expect("Not sure how this happened.");
+            let ba = Scanner::get_module(handle).expect("Module not found.") as usize;
+            println!("Found game process, exiting scanning thread.");
+            scanner_cpy
+              .deref()
+              .lock()
+              .expect("This should always have a value, even if its just default")
+              .base_address = ba;
+            scanner_cpy
+              .deref()
+              .lock()
+              .expect("This should always have a value, even if its just default")
+              .process_id = *pid;
+            run = false;
+            break;
+          }
+        }
+        let runtime = start.elapsed();
+        // If there is time left in the scheduler, sleep thread
+        if let Some(remaining) = process_scan_interval.checked_sub(runtime) {
+          thread::sleep(remaining);
+        }
+      }
     }
+  });
+}
+
+
+// Game scanning struct. Implements methods for reading values from game memory.
+struct Scanner {
+  process_id: usize,   // Base process id
+  base_address: usize, // Base address
+}
+
+impl Scanner {
+  fn new() -> Scanner {
+    let scanner = Scanner {
+      process_id: 1,
+      base_address: 1,
+    };
+    return scanner;
   }
-  Ok((module_id))
+
+  // Get the players current Gil
+  fn get_gil(&self) -> u32 {
+    // Static pointer
+    let bytes =
+      Scanner::read_memory(self.process_id as Pid, self.base_address + 0x01DD4358, 8).unwrap();
+    let str: String = hex::encode(bytes);
+    let address: usize = usize::from_str_radix(&str, 16).unwrap().try_into().unwrap();
+    // First offset
+    let bytes: Vec<u8> = Scanner::read_memory(self.process_id as Pid, address + 0x78, 8).unwrap();
+    let str: String = hex::encode(bytes);
+    let address: usize = usize::from_str_radix(&str, 16).unwrap().try_into().unwrap();
+    // Final offset
+    let bytes: Vec<u8> = Scanner::read_memory(self.process_id as Pid, address + 0xC, 4).unwrap();
+    let gil: u32 = u32::from_be_bytes(bytes.try_into().expect("Should always have a value"));
+    return gil;
+  }
+
+  // Read an array of bytes from a memory location
+  fn read_memory(pid: Pid, address: usize, size: usize) -> io::Result<Vec<u8>> {
+    let handle: ProcessHandle = pid.try_into()?;
+    let mut _bytes = copy_address(address, size, &handle)?;
+    _bytes.reverse(); // Flip the bytes so they are easier to work with (little endian to big)
+    Ok(_bytes)
+  }
+
+  // Get a win32 handle to a process by process ID
+  fn get_handle(pid: u32) -> io::Result<HANDLE> {
+    let handle: HANDLE;
+    unsafe {
+      handle = OpenProcess(
+        PROCESS_VM_READ | PROCESS_QUERY_INFORMATION,
+        BOOL::from(false),
+        pid,
+      );
+    }
+    Ok(handle)
+  }
+
+  // Get the base module of a process by process ID
+  fn get_module(handle: HANDLE) -> io::Result<u64> {
+    let mut hmods;
+    unsafe {
+      hmods = MaybeUninit::<[MaybeUninit<HINSTANCE>; 1024]>::uninit().assume_init();
+    }
+    let ptr = hmods.as_mut_ptr() as *mut HINSTANCE;
+    let mut cbneeded: u32 = 0;
+    let mut module_id: u64 = 0;
+    unsafe {
+      if K32EnumProcessModules(handle, ptr, 1024, &mut cbneeded) == BOOL::from(true) {
+        // Base module is always first
+        module_id = hmods[0].assume_init().0 as u64;
+      }
+    }
+    Ok(module_id)
+  }
 }
